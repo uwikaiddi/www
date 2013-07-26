e1e847313a4ea9ff7c7bfc01252701be85877120
Eric Holscher <eric@ericholscher.com>
2013-07-26 12:00:45 -0700

    Mess w/ static_url

--- docs/conf.py
+++ docs/conf.py
@@ -271,7 +271,7 @@ texinfo_documents = [
 
 
 websupport2_base_url = 'http://readthedocs.org/websupport'
-websupport2_static_url = 'http://media.readthedocs.org'
+websupport2_static_url = ''
 
 #siteurl = 'file://' + os.path.abspath('../var/docs/html') #won't work in windows
 feed_base_url = "http://docs.writethedocs.org/en/2013"
