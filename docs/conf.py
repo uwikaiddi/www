f4b31882c642e34214f27d100122043da7eef2e3
Eric Holscher <eric@ericholscher.com>
2013-07-26 11:39:59 -0700

    Point back at prod for testing.

--- docs/conf.py
+++ docs/conf.py
@@ -270,7 +270,8 @@ texinfo_documents = [
 
 
 
-#websupport2_base_url = 'https://readthedocs.org/websupport'
+websupport2_base_url = '//readthedocs.org/websupport'
+websupport2_static_url = '//media.readthedocs.org'
 
 #siteurl = 'file://' + os.path.abspath('../var/docs/html') #won't work in windows
 feed_base_url = "http://docs.writethedocs.org/en/2013"
